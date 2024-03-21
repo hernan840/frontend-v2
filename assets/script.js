62fc1da1e45f35c81c2aa838407ab7ee0849b135
hernan840 <hernanflores21@gmail.com>
2024-03-20 21:59:57 -0300

    se agrego la seccion footeh u se ajusto menu header fijo

--- assets/script.js
+++ assets/script.js
@@ -61,3 +61,17 @@ circles.forEach(elem=>{
 
 /* -------------selector de  proyectos*/
 var mixer = mixitup('.portfolio-galery');
+
+/* -------------activar  menu*/
+let menuLi = document.querySelectorAll('header ul li a');
+let section = document.querySelectorAll('section');
+
+function activeMenu(){
+    let len = section.length;
+    while(--len && window.scrollY +97 < section[len].offsetTop){}
+    menuLi.forEach(sec => sec.classList.remove("active"));
+    menuLi[len].classList.add("active");
+}
+
+activeMenu();
+window.addEventListener("scroll",activeMenu);
