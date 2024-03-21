0adcfd2ce2331fb5474a744acfe40bf1a37c3d82
hernan840 <hernanflores21@gmail.com>
2024-03-21 20:58:10 -0300

    se aplico el efecto parallax animacion

--- assets/script.js
+++ assets/script.js
@@ -95,3 +95,23 @@ menuIcon.onclick = ()=>{
     menuIcon.classList.remove("fa-xmark");
     navlist.classList.remove("open");
 }
+
+// efecto parallax-----------
+const observer = new IntersectionObserver((entries)=>{
+    entries.forEach((entry)=>{
+        if(entry.isIntersecting){
+            entry.target.classList.add("show-items");
+        }else{
+            entry.target.classList.remove("show-items");
+        }
+    });
+});
+
+const scrollScale = document.querySelectorAll(".scroll-scale");
+scrollScale.forEach((el)=>observer.observe(el));
+
+const scrollBottom = document.querySelectorAll(".scroll-bottom");
+scrollBottom.forEach((el)=>observer.observe(el));
+
+const scrollTop = document.querySelectorAll(".scroll-top");
+scrollTop.forEach((el)=>observer.observe(el));
